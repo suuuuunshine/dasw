d34b3b0058338225d27dd6fc68df095f842bbfb3
Sol González Parra <sol@reverscore.com>
2020-05-14 21:37:17 -0500

    todo roto

--- public/assets/js/server.js
+++ public/assets/js/server.js
@@ -13,7 +13,7 @@ const moment = require('moment');
 const express = require("express");
 const app = express();
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 const cors = require('cors');
 
 let usercountID = 3
